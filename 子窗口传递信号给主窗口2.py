7f0291a1a9f976988870d52b85204f6d6c9f7935
vichbb <hkmcomeon@hotmail.com>
2024-10-01 21:42:23 +0800

    多线程

--- 子窗口传递信号给主窗口2.py
+++ 子窗口传递信号给主窗口2.py
@@ -1,10 +1,10 @@
 import time
 
-from PySide6.QtCore import QThread, Signal
+from PySide6.QtCore import QThread, Signal,QObject
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
 
 
-class WorkThread(QThread):
+class WorkThread(QObject):
     signal = Signal(str)
 
     def __init__(self):
@@ -14,7 +14,6 @@ class WorkThread(QThread):
     def run(self):
         for i in range(10):
             self.signal.emit(str(i))
-            print(f"WorkThread emit:{i}")
             time.sleep(1)
 
 class MyWindow(QWidget):
@@ -22,13 +21,14 @@ class MyWindow(QWidget):
         super().__init__()
         self.lb = QLabel('当前值为:0')
 
-        self.workThread = WorkThread()
-        self.threadList = QThread()
-        self.workThread.moveToThread(self.threadList)
-        self.threadList.started.connect(self.workThread.run)
-        self.threadList.finished.connect(lambda :print("WorkThread finished"))
-        self.threadList.start()
 
+
+        self.workThread = WorkThread()
+        self.workThread.signal.connect(lambda x:self.lb.setText(f'当前值为:{x}'))
+        # self.workThread.finished.connect(lambda :print("WorkThread finished"))
+        # 线程结束后自动删除子线程,释放内存
+        self.workThread.finished.connect(self.workThread.deleteLater)
+        self.workThread.start()
 
         self.mainLayout = QVBoxLayout()
         self.mainLayout.addWidget(self.lb)
