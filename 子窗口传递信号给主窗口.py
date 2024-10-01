d55f8d9abca499088b5db3b68e829c178b432e92
vichbb <hkmcomeon@hotmail.com>
2024-10-01 18:58:10 +0800

    Timer

---
+++ 子窗口传递信号给主窗口.py
@@ -0,0 +1,40 @@
+import time
+
+from PySide6.QtCore import QThread, Signal
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+
+
+class WorkThread(QThread):
+    signal = Signal(str)
+
+    def __init__(self):
+        super().__init__()
+        print("WorkThread init")
+
+    def run(self):
+        for i in range(10):
+            self.signal.emit(str(i))
+            time.sleep(1)
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.lb = QLabel('当前值为:0')
+
+        self.workThread = WorkThread()
+        self.workThread.signal.connect(lambda x:self.lb.setText(f'当前值为:{x}'))
+        # self.workThread.finished.connect(lambda :print("WorkThread finished"))
+        # 线程结束后自动删除子线程,释放内存
+        self.workThread.finished.connect(self.workThread.deleteLater)
+        self.workThread.start()
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.lb)
+        self.setLayout(self.mainLayout)
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    myWin = MyWindow()
+    myWin.show()
+    app.exec()
